9053796de25114fb46e6b23b8c92ece8b608699f
andyla714 <andyla714@gmail.com>
2024-12-07 21:51:45 +0000

    First commit -> Just .html (everything works, but without back-end == Firestore && Authentication)

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCzIziOAUtyFqZMNFPxIaH15nyNv0972hA",
+    authDomain: "pc-store-9.firebaseapp.com",
+    databaseURL: "https://pc-store-9-default-rtdb.firebaseio.com",
+    projectId: "pc-store-9",
+    storageBucket: "pc-store-9.firebasestorage.app",
+    messagingSenderId: "34549374359",
+    appId: "1:34549374359:web:f29bb367d981c97ae478d2"
+  };
